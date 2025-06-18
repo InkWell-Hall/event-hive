12385ed6b429dc4bd73c79c13284ce9be183eb32
Kez-hub1 <ampomahkeziah@gmail.com>
2025-06-18 14:59:18 +0000

    Trending Colleges Integration

--- src/api/client.js
+++ src/api/client.js
@@ -3,3 +3,11 @@ import axios from "axios";
 export const apiClient = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
 });
+
+
+export const apifetcher = async (url) => {
+  const response = await apiClient.get(url);
+  return response.data;
+};
+
+export const imageBaseUrl = import.meta.env.VITE_IMAGE_BASE_URL;
